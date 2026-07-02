04d0586b65d3f782a9ac391039f21e6bfe00d200
ManshiMishra <99650618+ManshiMishra@users.noreply.github.com>
2026-07-02 16:39:06 +0530

    changes

--- notification-service/src/main/java/com/notification/consumer/UserEventConsumer.java
+++ notification-service/src/main/java/com/notification/consumer/UserEventConsumer.java
@@ -2,15 +2,17 @@ package com.notification.consumer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
 
 import com.common.dto.UserEvent;
 import com.notification.service.EmailService;
 
+@Service
 public class UserEventConsumer {
     @Autowired
     private EmailService emailService;
 
-    @KafkaListener(topics = "user-welcome", groupId = "notificarion-group")
+    @KafkaListener(topics = "user-welcome", groupId = "notification-group")
     public void consume(UserEvent event){
         emailService.sendWelcome(event);
     }
